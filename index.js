9ea1b40c28d1dbc75bcbc7f22e60d7f1b6dc6c21
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-03-01 21:20:13 +0300

    connected project to mongodb

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 7000;
 
 const app = express();
+
+app.use(express.json());
 app.use(cors());
 
-const appStart = async () => {
+app.post('/', (req, res) => {
+	console.log('req ==>', req.body);
+	res.status(200).json('server works');
+});
+
+const startApp = async () => {
 	try {
+		await mongoose.connect(process.env.DB_URL);
+
 		app.listen(PORT, () => {
 			console.log(`Server started on port: ${PORT}`);
 		});
@@ -16,4 +28,4 @@ const appStart = async () => {
 	}
 };
 
-appStart();
+startApp();
